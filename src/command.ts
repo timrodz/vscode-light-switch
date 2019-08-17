796214c6812c6bf3e221d5bb1f84f890a44257f4
Juan Alejandro Morais <timrodz@icloud.com>
2019-08-17 14:53:51 +1200

    Format & add package commands

--- src/command.ts
+++ src/command.ts
@@ -1,18 +1,42 @@
 // The command has been defined in the package.json file
 // Now provide the implementation of the command with registerCommand
 // The commandId parameter must match the command field in package.json
-import * as vscode from "vscode";
-import { bind } from "./util/extensionHandle";
-import { getCurrentDate } from "./util/time";
+import * as vscode from 'vscode';
+import { bind } from './util/extensionHandle';
+import { getCurrentDate } from './util/time';
 
-export function registerCommandSwitchLights(): vscode.Disposable {
-    return bind("switchLights", () => {
-        vscode.window.showWarningMessage("Switching lights!");
+// SWITCH
+
+export function registerCommandSwitch(): vscode.Disposable {
+    return bind('switch', () => {
+        vscode.window.showWarningMessage('Switching lights!');
     });
 }
 
-export function registerCommandSetTime(): vscode.Disposable {
-    return bind("displayTime", () => {
+// SET MODE
+
+export function registerCommandSetModeDark(): vscode.Disposable {
+    return bind('switch', () => {
+        vscode.window.showWarningMessage('Switching lights!');
+    });
+}
+
+export function registerCommandSetModeLight(): vscode.Disposable {
+    return bind('switch', () => {
+        vscode.window.showWarningMessage('Switching lights!');
+    });
+}
+
+// SET TIME
+export function registerCommandSetTimeModeDark(): vscode.Disposable {
+    return bind('displayTime', () => {
+        const time = getCurrentDate();
+        vscode.window.showInformationMessage(`The time is: ${time}`);
+    });
+}
+
+export function registerCommandSetTimeModeLight(): vscode.Disposable {
+    return bind('displayTime', () => {
         const time = getCurrentDate();
         vscode.window.showInformationMessage(`The time is: ${time}`);
     });
