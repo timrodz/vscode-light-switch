ad615e4a6d9680bc82b8e33d2fbe0a85df037f73
Giovanni Almeida <gronniea@gmail.com>
2019-10-18 15:10:04 +1100

    Fixing error to find Constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,8 +1,6 @@
-const Constants = Object.freeze({
+export const Constants = Object.freeze({
   THEME_DAY: 'themeDay',
   LIGHT_SWITCH: 'lightSwitch',
   INTERVAL_MINUTES: 10,
   INTERVAL_FORMULA: 60 * 1000
 });
-
-module.exports = Constants;
