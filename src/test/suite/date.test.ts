712ffb50b76309117190f56758de873e86ad641c
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-18 23:14:10 +1300

    Replace moment.js with day.js; update date tests

--- src/test/suite/date.test.ts
+++ src/test/suite/date.test.ts
@@ -1,9 +1,22 @@
 import * as assert from 'assert';
-import * as moment from 'moment';
+import * as dayjs from 'dayjs';
+import { canSwitchToNightTheme } from '../../util/date';
 
-suite('date', () => {
-  const time = moment('17:00', 'hh:mm');
-  assert(time.isAfter(moment('00:00', 'hh:mm')));
-  assert(!time.isAfter(moment('24:00', 'hh:mm')));
-  assert(!time.isAfter(moment('17:00', 'hh:mm')));
+suite('Date Test Suite', () => {
+  test('Time conversions', () => {
+    const timeNow = dayjs('12-25-1995 17:00', 'MM-DD-YYYY HH:mm');
+    assert(timeNow.isSame(dayjs('12-25-1995 17:00', 'MM-DD-YYYY HH:mm')));
+    assert(timeNow.isBefore(dayjs('12-25-1995 17:01', 'MM-DD-YYYY HH:mm')));
+    assert(timeNow.isAfter(dayjs('12-25-1995 00:00', 'MM-DD-YYYY HH:mm')));
+    assert(timeNow.isBefore(dayjs('12-25-1995 24:00', 'MM-DD-YYYY HH:mm')));
+  });
+
+  test('Setting time through the extension', () => {
+    assert(!canSwitchToNightTheme('17:00', '16:00'));
+    assert(canSwitchToNightTheme('17:00', '17:00'));
+    assert(canSwitchToNightTheme('17:00', '17:01'));
+    assert(!canSwitchToNightTheme('17:00', '00:00'));
+    assert(canSwitchToNightTheme('17:00', '24:00'));
+    assert(canSwitchToNightTheme('17:00', '23:59'));
+  });
 });
--- src/util/date.ts
+++ src/util/date.ts
@@ -1,11 +1,18 @@
-import * as moment from 'moment';
+import * as dayjs from 'dayjs';
 import { getExtensionConfig } from './workspace';
 
-export function canSwitchToNightTheme(defaultTime: string = '17:00'): boolean {
+function convertDate(today: string, time: string): dayjs.Dayjs {
+  return dayjs(`${today} ${time}`, 'YYYY-MM-DD HH:mm');
+}
+
+export function canSwitchToNightTheme(
+  defaultTime: string = '17:00',
+  currentTime: string = ''
+): boolean {
   const extensionConfig = getExtensionConfig();
   const nightTime: string = extensionConfig.get('nightTime', defaultTime);
-  const currentTime: string = moment().format('hh:mm');
-  const time = moment(currentTime, 'hh:mm');
-  const afterTime = moment(nightTime, 'hh:mm');
-  return time.isAfter(afterTime);
+  const today = dayjs().format('YYYY-MM-DD');
+  const timeNow = currentTime ? convertDate(today, currentTime) : dayjs();
+  const timeToSwitch = convertDate(today, nightTime);
+  return timeNow.isSame(timeToSwitch) || timeNow.isAfter(timeToSwitch);
 }
