eaddb2cc771d043425c95b27b48bb0b3a9945b6e
Juan Alejandro Morais <timrodz@icloud.com>
2020-02-18 23:14:41 +1300

    Remove setTheme.test & vsc-extension.quickstart.md

--- src/test/suite/setTheme.test.ts
+++
@@ -1,8 +0,0 @@
-import * as assert from 'assert';
-import { before } from 'mocha';
-import * as vscode from 'vscode';
-import setTheme from '../../commands/setTheme';
-
-suite('setTheme', () => {
-    
-});
